448b22b4c3465f9247d676d593d5aa5376eb2ff2
lilawalker <lilawlker@gmail.com>
2020-07-02 15:20:52 +0100

    Add link to download CV

--- src/Contact.js
+++ src/Contact.js
@@ -29,21 +29,30 @@ class Contact extends React.Component {
 
     return (
       <div>
-        <div className="text-center pt-12 pb-6 font-logo text-xl text-pink-700">
+        <div className="text-center py-12 font-logo text-xl text-pink-700">
           Find Me Below
         </div>
-        <div className="flex justify-evenly m-auto p-4 w-1/3">
-          {
-            contact.map((details, index) => {
-              return (
-                <div key={index}>
-                  <a href={details.link} target="blank">
-                    <img src={details.logo} alt="Logo" className="h-16" />
-                  </a>
-                </div>
-              );
-            })
-          }
+
+        <ul className="flex justify-evenly m-auto p-4 w-1/3">
+          {contact.map((details, index) => {
+            return (
+              <li key={index}>
+                <a href={details.link} target="blank">
+                  <img src={details.logo} alt="Logo" className="h-16" />
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <div className="text-center mt-12">
+          <a
+            href="/LilaWalkerCV.pdf"
+            download
+            className="font-logo text-pink-500 text-xs bg-pink-200 hover:bg-pink-400 active:bg-pink-300 rounded-full px-3 py-1"
+          >
+            Download CV
+          </a>
         </div>
       </div>
     );
