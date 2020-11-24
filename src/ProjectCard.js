393b147b7e09f9af07fcb097637096323f9eb568
Lila Walker <lilawlker@gmail.com>
2020-11-24 18:41:49 +0000

    Refactor Project components using hooks

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -2,79 +2,72 @@ import React from 'react';
 import link from './logos/Link';
 import GitHubLogo from './logos/GitHubLogo';
 
-class ProjectCard extends React.Component {
+const ProjectCard = ({ project }) => {
+  return (
+    <div className="w-full lg:flex my-12 border-pink-200 border rounded bg-gray-100">
+      <div className="min-h-48 lg:h-auto lg:w-1/3 w-full flex items-center bg-pink-200">
+        {project.image !== "" && (
+          <img src={project.image} alt="img" className="block mx-auto" />
+        )}
+      </div>
 
-  render() {
-
-    const project = this.props.project;
-
-    return (
-      <div className="w-full lg:flex my-12 border-pink-200 border rounded bg-gray-100">
-        <div className="min-h-48 lg:h-auto lg:w-1/3 w-full flex items-center bg-pink-200">
-          {project.image !== "" && (
-            <img src={project.image} alt="img" className="block mx-auto" />
-          )}
+      <div className="w-full lg:w-2/3 p-4 leading-normal">
+        <div className="font-bold text-2xl mb-2 font-logo text-pink-700">
+          {project.title}
         </div>
 
-        <div className="w-full lg:w-2/3 p-4 leading-normal">
-          <div className="font-bold text-2xl mb-2 font-logo text-pink-700">
-            {project.title}
-          </div>
+        <div className="inline-flex pr-6 text-pink-500">
+          <svg
+            className="fill-current w-3 h-3 mr-1 mt-1"
+            xmlns={GitHubLogo.xmlns}
+            viewBox={GitHubLogo.viewBox}
+          >
+            {GitHubLogo.path}
+          </svg>
+          <a
+            href={project.githubLink}
+            className="text-sm pl-1 hover:text-pink-400 active:text-pink-300"
+            target="blank"
+          >
+            GitHub
+          </a>
+        </div>
 
-          <div className="inline-flex pr-6 text-pink-500">
+        {project.demoLink !== "" && (
+          <div className="inline-flex text-pink-500">
             <svg
               className="fill-current w-3 h-3 mr-1 mt-1"
-              xmlns={GitHubLogo.xmlns}
-              viewBox={GitHubLogo.viewBox}
+              xmlns={link.xmlns}
+              viewBox={link.viewBox}
             >
-              {GitHubLogo.path}
+              {link.path}
             </svg>
             <a
-              href={project.githubLink}
+              href={project.demoLink}
               className="text-sm pl-1 hover:text-pink-400 active:text-pink-300"
               target="blank"
             >
-              GitHub
+              Demo
             </a>
           </div>
+        )}
 
-          {project.demoLink !== "" && (
-            <div className="inline-flex text-pink-500">
-              <svg
-                className="fill-current w-3 h-3 mr-1 mt-1"
-                xmlns={link.xmlns}
-                viewBox={link.viewBox}
-              >
-                {link.path}
-              </svg>
-              <a
-                href={project.demoLink}
-                className="text-sm pl-1 hover:text-pink-400 active:text-pink-300"
-                target="blank"
-              >
-                Demo
-              </a>
-            </div>
-          )}
+        <p className="text-gray-700 text-md py-2">{project.description}</p>
 
-          <p className="text-gray-700 text-md py-2">{project.description}</p>
-
-          <ul className="mt-4">
-            {project.techStack.map((tech, index) => {
-              return (
-                <li key={index} className="py-1 inline">
-                  <span className="inline-block bg-pink-200 rounded-full px-3 py-1 mb-2 text-xs font-semibold text-gray-700 mr-2">
-                    {tech}
-                  </span>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <ul className="mt-4">
+          {project.techStack.map((tech, index) => {
+            return (
+              <li key={index} className="py-1 inline">
+                <span className="inline-block bg-pink-200 rounded-full px-3 py-1 mb-2 text-xs font-semibold text-gray-700 mr-2">
+                  {tech}
+                </span>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    );
-  }
-
+    </div>
+  );
 }
 
 export default ProjectCard;
---
+++ src/data/projectData.js
@@ -0,0 +1,148 @@
+import Volunteer from '../images/VolunteerMatchingScreenshot.png'
+import MySite from '../images/MySiteScreenshot.png'
+import MixTape from '../images/MixTapeScreenshot.png'
+import Bank from '../images/BankScreenshot.png'
+import Acebook from '../images/AcebookScreenshot.png'
+import Chitter from "../images/ChitterScreenshot.png";
+import Instagram from "../images/InstagramScreenshot.png";
+
+const projectData = [
+  {
+    title: "Volunteer Matching",
+    githubLink: "https://github.com/Age-UK-Voluteer-Matching/Match-App",
+    demoLink: "https://volunteer-match-app.herokuapp.com",
+    description:
+      "An app developed at Hack for Heroes hackathon for Age UK's brief - helping to create relationships between volunteers and senior community members. The app won second place in the competition!",
+    techStack: [
+      "Ruby",
+      "Rails",
+      "PostgreSQL",
+      "ImageMagick",
+      "AWS S3",
+      "RESTful",
+      "Travis CI",
+      "Heroku",
+      "Pair Programming",
+      "Agile Development",
+      "Hackathon",
+    ],
+    image: Volunteer,
+  },
+  {
+    title: "Personal Website",
+    githubLink: "https://github.com/lilawalker/personal-website",
+    demoLink: "",
+    description: "You're looking at it now!",
+    techStack: ["JavaScript", "React", "HTML", "Tailwind CSS", "Netlify"],
+    image: MySite,
+  },
+  {
+    title: "MixTape",
+    githubLink: "https://github.com/lilawalker/mix-tape",
+    demoLink: "https://mix-tape-makers.herokuapp.com/signin",
+    description: "A collaborative playlist generator using Spotify's API.",
+    techStack: [
+      "Ruby",
+      "Rails",
+      "JavaScript",
+      "React",
+      "PostgreSQL",
+      "CSS",
+      "RSpec",
+      "Rubocop",
+      "SimpleCov",
+      "ESLint",
+      "Spotify API",
+      "Travis CI",
+      "Heroku",
+      "Pair Programming",
+      "Agile Development",
+    ],
+    image: MixTape,
+  },
+  {
+    title: "Instagram Clone",
+    githubLink: "https://github.com/lilawalker/instagram-challenge-lw",
+    demoLink: "https://instagram-challenge-lw.herokuapp.com/",
+    description:
+      "An Instagram clone featuring user authentication, image uploads, comments and likes.",
+    techStack: [
+      "Ruby",
+      "Rails",
+      "PostgreSQL",
+      "ImageMagick",
+      "AWS S3",
+      "Capybara",
+      "RSpec",
+      "Rubocop",
+      "SimpleCov",
+      "OOP",
+      "TDD",
+      "MVC",
+      "RESTful",
+      "Travis CI",
+      "Heroku",
+    ],
+    image: Instagram,
+  },
+  {
+    title: "Chitter",
+    githubLink: "https://github.com/lilawalker/chitter-challenge",
+    demoLink: "",
+    description: "A Twitter clone featuring user authentication and posts.",
+    techStack: [
+      "Ruby",
+      "Sinatra",
+      "PostgreSQL",
+      "ActiveRecord",
+      "CSS",
+      "Capybara",
+      "RSpec",
+      "Rubocop",
+      "SimpleCov",
+      "OOP",
+      "TDD",
+      "MVC",
+      "RESTful",
+      "Travis CI",
+    ],
+    image: Chitter,
+  },
+  {
+    title: "Bank Account",
+    githubLink: "https://github.com/lilawalker/bank-tech-test",
+    demoLink: "",
+    description:
+      "Practice tech test to demonstrate OOP and TDD skills. IRB app to deposit/withdraw funds, check balance and print a statement of account.",
+    techStack: ["Ruby", "IRB", "RSpec", "Rubocop", "SimpleCov", "OOP", "TDD"],
+    image: Bank,
+  },
+  {
+    title: "Acebook",
+    githubLink: "https://github.com/lilawalker/acebook-smooth-railing",
+    demoLink: "https://acebook-smooth-railing.herokuapp.com/users/sign_in",
+    description:
+      "A Facebook clone featuring user authentication, posts and comments.",
+    techStack: [
+      "Ruby",
+      "Rails",
+      "PostgreSQL",
+      "CSS",
+      "Capybara",
+      "RSpec",
+      "Rubocop",
+      "SimpleCov",
+      "OOP",
+      "TDD",
+      "MVC",
+      "RESTful",
+      "Travis CI",
+      "Heroku",
+      "Pair Programming",
+      "Agile Development",
+    ],
+    image: Acebook,
+  },
+];
+
+export default projectData;
